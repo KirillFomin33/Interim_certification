c6c3484750067a35e224e6155443c7f7baa908e7
Kirill.fo <kirill.fo2012@yandex.ru>
2024-03-19 19:41:39 +0300

    Add Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 21);
+}
+
+void ReversArray(int[] array, int count)
+{
+    if (count == 0) return;
+    Console.Write($"{array[count - 1]} ");
+    ReversArray(array, count - 1);
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.Write("Перевернутый массив: [");
+ReversArray(array, array.Length);
+Console.Write("]");
